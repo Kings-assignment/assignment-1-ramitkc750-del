5a7022bc5ebf384ce20a3b3f1b60cc05ae8bc5fb
unknown <ramitkc750@gmail.com>
2026-02-05 12:00:20 +0545

    q18

--- src/q18.c
+++ src/q18.c
@@ -1,1 +1,13 @@
 // Write a C expression that calculates the sum of the squares of three different numbers.
+#include <stdio.h>
+
+int main() {
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    
+    int result = (a * a) + (b * b) + (c * c); 
+    
+    printf("Sum of squares: %d", result);
+    return 0;
+}
